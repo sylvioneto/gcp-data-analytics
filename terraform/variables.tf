40d8235714ffb950443dd527cb2ad2b06e11f386
sylvio.pedroza <sylvio.pedroza@gmail.com>
2022-08-19 16:57:25 -0300

    test

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -20,5 +20,5 @@ variable "region" {
 
 variable "gcp_ingest_api_image" {
   description = "Container image to use on Cloud Run ingest API"
-  default     = "us-central1-docker.pkg.dev/syl-data-analytics/docker-repo/gcp-ingest-api:9b6149a6-1623-4cb8-920f-31d4630d0e52"
+  default     = "us-central1-docker.pkg.dev/syl-data-analytics/docker-repo/gcp-ingest-api:05c3f04e-e3b5-41c7-a028-6b3eae3e4fab"
 }
