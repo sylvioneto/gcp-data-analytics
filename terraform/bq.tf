85fba76b8530ada82a2f30e421a385f38565d728
sylvio.pedroza <sylvio.pedroza@gmail.com>
2022-09-29 13:26:58 -0300

    add unknow table

--- terraform/bq.tf
+++ terraform/bq.tf
@@ -7,8 +7,54 @@ resource "google_bigquery_dataset" "raw" {
 
 resource "google_bigquery_table" "raw_order_events" {
   dataset_id          = google_bigquery_dataset.raw.dataset_id
-  table_id            = "order_events"
-  description         = "Store events streamed from order-events-to-bigquery subscription"
+  table_id            = "order_event"
+  description         = "Store order events streamed from order-events-to-bigquery subscription"
+  deletion_protection = false
+
+  time_partitioning {
+    type  = "DAY"
+    field = "publish_time"
+  }
+
+  labels = local.resource_labels
+
+  schema = <<EOF
+[
+  {
+    "name": "subscription_name",
+    "type": "STRING",
+    "mode": "NULLABLE"
+  },
+  {
+    "name": "message_id",
+    "type": "STRING",
+    "mode": "NULLABLE"
+  },
+  {
+    "name": "publish_time",
+    "type": "TIMESTAMP",
+    "mode": "NULLABLE"
+  },
+  {
+    "name": "data",
+    "type": "STRING",
+    "mode": "NULLABLE"
+  },
+    {
+    "name": "attributes",
+    "type": "STRING",
+    "mode": "NULLABLE"
+  }
+]
+EOF
+
+}
+
+
+resource "google_bigquery_table" "raw_unknown" {
+  dataset_id          = google_bigquery_dataset.raw.dataset_id
+  table_id            = "unknown_entity"
+  description         = "Store unknown events received by the Ingest API"
   deletion_protection = false
 
   time_partitioning {
--- terraform/pubsub.tf
+++ terraform/pubsub.tf
@@ -15,3 +15,15 @@ resource "google_pubsub_subscription" "order_to_bq_sub" {
   }
 }
 
+resource "google_pubsub_subscription" "unknown_to_bq_sub" {
+  name   = "unknown-to-bigquery"
+  topic  = google_pubsub_topic.ingest_api.name
+  labels = local.resource_labels
+  filter = "attributes.entity=\"unknown\""
+
+  bigquery_config {
+    table          = "${data.google_project.project.name}.${google_bigquery_dataset.raw.dataset_id}.${google_bigquery_table.raw_order_events.table_id}"
+    write_metadata = true
+  }
+}
+
